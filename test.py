7b4205adb9604cc129abd3a75484faa3832100a1
PerterJay <jiyiping@gmail.com>
2013-05-21 11:19:05 +0800

    tfs_restful.py: read/write/delete/metadate api in python test.py: test case, see them as api usage

---
+++ test.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+#
+# test cases for tfs restful in python 
+#
+
+import sys
+import urllib2
+import httplib
+import random
+sys.path.append('./')
+
+from tfs_restful import TFS_Restful
+
+def test_get_meta(client, tfsname):
+    print 'test get meta'
+    print client.get_tfs_meta(tfsname, type=1)
+
+def test_get_data(client, tfsname):
+    print 'test get data'
+    rsp = client.get_tfs_data(tfsname)
+    if rsp:
+        print len(rsp)
+
+def test_write(client):
+    print 'test write'
+    data = 'test tfs restful write, by zhiqi 2013-05-20 '
+    rsp = client.write_tfs(data, suffix='.outimg', simple_name=0)
+    print 'write rsp: ', rsp
+    st = eval(rsp) 
+    tfs = st['TFS_FILE_NAME']
+    print 'meta: ', client.get_tfs_meta(tfs)
+    print 'data: ', client.get_tfs_data(tfs)
+
+def test_delete(client):
+    print 'test delete'
+    data = 'test tfs restful delete, by zhiqi 2013-05-20 '
+    rsp = client.write_tfs(data, suffix='.outimg')
+    st = eval(rsp)
+    tfs = st['TFS_FILE_NAME']
+    print tfs
+    print client.del_tfs(tfs, 0)
+    print client.get_tfs_meta(tfs, type=1, suffix='.outimg')
+
+if __name__ == "__main__":
+    # appkey, keep it secret 
+    appkey = sys.argv[1] 
+    client = TFS_Restful( appkey)
+    print client.req_count, client.TfsProxyServer
+
+    tfsname = "T4WhVyXcBXXXXXXXXX"
+    test_get_meta(client, tfsname)
+
+    #test_get_data(client, tfsname)
+
+    #test_write(client)
+
+    #test_delete(client)
+
---
+++ tfs_restful.py
@@ -0,0 +1,122 @@
+#!/usr/bin/python
+
+# TFS Restful in python
+# test passed python2.4, python2.6
+
+import sys
+import urllib2
+import httplib
+import random
+
+class TFS_Restful:
+    def __init__(self, appkey):
+        self.appkey = appkey
+        self.web_root_server = 'restful-store.vip.tbsite.net:3800' 
+        self.req_count = 0
+        self.TfsProxyServer = []
+        self.set_TfsProxyServer()
+
+    def do_webroot_request(self):
+        url = "http://%s/v1/tfs.list" % (self.web_root_server)
+        conn = httplib.HTTPConnection(self.web_root_server)
+        conn.request("GET", url)
+        rs = conn.getresponse()
+        data = rs.read()
+        conn.close()
+        return  data
+
+
+    def do_tfs_request(self, method, url, data=None, headers={}):
+        try :
+            proxy = self.TfsProxyServer[random.randint(0, len(self.TfsProxyServer))]
+            conn = httplib.HTTPConnection(proxy)
+            conn.request(method, url, data, headers)
+            rs = conn.getresponse()
+            if rs.status != 200:
+                conn.close()
+                return None
+            data  = rs.read()
+            conn.close()
+            self.req_count -= 1
+            if self.req_count <= 0 :
+                self.set_TfsProxyServer()
+            return data 
+        except httplib.HTTPException:
+            return None
+        except :
+            return None 
+
+    def parse_server(self, data):
+        if not data:
+            return None, None
+        if data == '':
+            return None, None
+        lines = data.split('\n')
+        try :
+            req_count = int(lines[0])
+        except ValueError:
+            print >>sys.stderr, 'Error: tfs proxy server parse failed!'
+            return None, None
+        if req_count <= 0 :
+            print >>sys.stderr, 'Error: request count <=0 !'
+            return None, None
+
+        proxy_list = []
+        for i in lines[1:]:
+            if i != '' : 
+                proxy_list.append(i)
+        return req_count, proxy_list 
+
+    def set_TfsProxyServer(self):
+        rsp = self.do_webroot_request()
+        req_count, proxy_list = self.parse_server(rsp)
+        self.req_count = req_count 
+        self.TfsProxyServer = proxy_list
+        return req_count, proxy_list 
+
+    # GET /v1/appkey/metadata/TfsFileName HTTP/1.1
+    # type=1, force to read tfs meta in hide or deleted status
+    def get_tfs_meta(self, tfsname, suffix=None, type=0):
+        if suffix:
+            tfsname = tfsname + suffix
+        url = "/v1/%s/metadata/%s" %  (self.appkey, tfsname)
+        if type == 1:
+            url = "%s?type=1" % (url)
+        rsp = self.do_tfs_request('GET', url)
+        return rsp
+
+    # GET /v1/appkey/TfsFileName HTTP/1.1 
+    def get_tfs_data(self, tfsname):
+        url = "/v1/%s/%s" %  (self.appkey, tfsname)
+        rsp = self.do_tfs_request('GET', url)
+        return rsp
+        pass
+   
+    # suffix=, tfs will set the suffix
+    # simple_name=1, make reading require suffix
+    def write_tfs(self, data, suffix=None, simple_name=0):
+        query = []
+        if suffix:
+            param = "suffix=%s" % suffix
+            query.append(param)
+        if simple_name == 1:
+            param = "simple_name=1"
+            query.append(param)
+
+        if query :
+            url = "/v1/%s?%s" %  (self.appkey, '&'.join(query))
+        else :
+            url = "/v1/%s" %  (self.appkey)
+
+        rsp = self.do_tfs_request('POST', url, data)
+        return rsp
+    
+    # hide=1, set tfs status to be hidden beside deleted
+    def del_tfs(self, tfsname, hide=0):
+        url = "/v1/%s/%s" %  (self.appkey, tfsname)
+        if hide == 0:
+            url = "%s?hide=1" % (url)
+        rsp = self.do_tfs_request('DELETE', url)
+        return rsp
+
+
